da5ebc508ba68d1ebd87513e476acf77a0f0be37
tom statter <dev@autotelik.co.uk>
2019-04-02 20:40:23 +0200

    performance issues

--- app/controllers/yams_core/album/albums_controller.rb
+++ app/controllers/yams_core/album/albums_controller.rb
@@ -15,12 +15,15 @@ module YamsCore
     layout 'application_with_player', only: %i[index show]
 
     def index
- 
+
+      # HTML or JS is to Render the Audio Player, a JSON format is to Render the Playlist and actual audio data
+      @datashift_audio_json_settings = datashift_audio_settings if request.format.html? || request.format.js?
+
       # Technique to generate same list for a certain time - driven by the cookie expire time
       # seed_val = Track.connection.quote(cookies[:rand_seed])
       seed_val = rand
       Album.connection.execute("select setseed(#{seed_val})")
-      @albums = Album.eager_load(:cover, :user).published.order('random()').page(params[:page]).per(30)
+      @albums = Album.includes(cover: { image_attachment: :blob } ).eager_load(:tracks, :user).published.order('random()').page(params[:page]).per(30)
 
       respond_to do |format|
         format.html {}
@@ -112,6 +115,32 @@ module YamsCore
     def album_params
       params.require(:album).permit(:title, :description, :published_state, :user_id, tag_list: [], cover_attributes: %i[id image])
     end
+
+    def datashift_audio_settings
+      Jbuilder.encode do |json|
+        json.datashift_audio do
+
+          json.service do
+            if current_user     # radio stream can be accessed by non signed in visitors
+              json.user_token   current_user.id
+              json.client_token '0987654321' # TODO: - add tokens to devise
+            end
+          end
+
+          json.settings do
+            json.autoplay true
+          end
+
+          json.waveform do
+            json.wave_color 'white'
+            json.progress_color 'blue'
+            json.cursor_color 'purple'
+            json.bar_width 'w-100'
+          end
+        end
+      end
+    end
+
   end
 
 
